b22a4a96ad7b18d60bac1bc9609f162dcab70035
unknown <payamyarandi98@gmail.com>
2020-11-07 11:59:14 +0330

    Tutorial serializer and viewset added

--- app/classroom/serializers.py
+++ app/classroom/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from core.models import Comment
+from core.models import Comment, Tutorial
 
 
 class CommentSerializer(serializers.ModelSerializer):
@@ -9,4 +9,13 @@ class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
         fields = ('id', 'text', 'like', 'user')
+        read_only_fields = ('id',)
+
+
+class TutorialSerializer(serializers.ModelSerializer):
+    """Serialize a video"""
+
+    class Meta:
+        model = Tutorial
+        fields = ('id', 'video', 'description')
         read_only_fields = ('id',)
--- app/classroom/views.py
+++ app/classroom/views.py
@@ -1,21 +1,34 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, mixins
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from core.models import Comment, User
+from core.models import Comment, Tutorial
 
 from classroom import serializers
 
 
-class AdvertisementViewSet(viewsets.ModelViewSet):
-    """Manage comments in classrooms"""
-    serializer_class = serializers.CommentSerializer
-    queryset = Advertisement.objects.all()
+class BaseClassroomAttrViewSet(viewsets.GenericViewSet,
+                     mixins.ListModelMixin,
+                     mixins.CreateModelMixin):
+    """Manage classroom attributes in db"""
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated)
 
     def get_queryset(self):
-        return self.queryset.all()
+        """Return objects for current authenticated users only"""
+        return self.queryset.filter(user=self.request.user).order_by('-name')
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+
+class CommentViewSet(BaseClassroomAttrViewSet):
+    """Manage comments in the database"""
+    queryset = Comment.objects.all()
+    serializer_class = serializers.CommentSerializer
+
+
+class TutorialViewSet(BaseClassroomAttrViewSet):
+    """Manage tutorials in the database"""
+    queryset = Tutorial.objects.all()
+    serializer_class = serializers.TutorialSerializer
