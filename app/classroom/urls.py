8d81b6bdba26aa5c83a5aa867a0644a1def3eded
unknown <payamyarandi98@gmail.com>
2020-11-11 00:12:58 +0330

    Classroom extra urls deleted

--- app/classroom/urls.py
+++ app/classroom/urls.py
@@ -5,8 +5,6 @@ from classroom import views
 
 
 router = DefaultRouter()
-router.register('my_classrooms', views.ManageClassroomViewSet)
-router.register('create_classroom', views.CreateClassroomViewSet)
 router.register('classrooms', views.ClassroomViewSet)
 router.register('tutorials', views.TutorialViewSet)
 router.register('comments', views.CommentViewSet)
